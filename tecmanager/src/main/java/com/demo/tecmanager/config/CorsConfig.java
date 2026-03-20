ddbba47497cf9a95d80f4a63d4a28dce07addb05
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2026-03-19 22:19:17 -0500

    v2.4

--- tecmanager/src/main/java/com/demo/tecmanager/config/CorsConfig.java
+++ tecmanager/src/main/java/com/demo/tecmanager/config/CorsConfig.java
@@ -17,8 +17,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
             "http://localhost:3000",
             "http://localhost:5173",
-            "https://tecmanager-back.onrender.com",
-            "https://tecmanager.onrender.com"
+            "http://tecmanager-app.onrender.com"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
